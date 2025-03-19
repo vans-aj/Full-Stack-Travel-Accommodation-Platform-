d2df69e99d42e1679c895aa8d8fe8dff547f3751
vans-aj <vnshajrawat951@gmail.com>
2025-03-20 00:06:55 +0530

    created view

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const ejs = require("ejs");
 const port = 8080;
 const Listing = require("./models/listing.js");
+const path = require("path");
 //conecting to mongodb
 
 main().then(()=>{
@@ -16,23 +17,32 @@ main().then(()=>{
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
-
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname , "views"));
 app.listen(port,()=>{
     console.log("server is listening at 8080");
 });
 
-app.get("/testlisting",async (req,res)=>{
-    let list = new Listing({
-        title : "my new villa",
-        description: "buy my beach",
-        price : 1300,
-        location : "clement town",
-        country : "india"
-    });
-    await list.save(); 
-    console.log("list saved successfully");
-    res.send("successfully added");
-});
+app.get("/listings",async (req,res)=>{
+    const alllist = await Listing.find({});
+    res.render("listing/index.ejs" , {alllist});
+})
+
+app.get("/listings/:id",async (req,res)=>{
+    
+})
+// app.get("/testlisting",async (req,res)=>{
+//     let list = new Listing({
+//         title : "my new villa",
+//         description: "buy my beach",
+//         price : 1300,
+//         location : "clement town",
+//         country : "india"
+//     });
+//     await list.save(); 
+//     console.log("list saved successfully");
+//     res.send("successfully added");
+// });
 
 app.get('/',(req,res)=>{
     res.send("hello this is home page");
--- models/listing.js
+++ models/listing.js
@@ -7,10 +7,9 @@ const newschema = new schema({
         required: true // it means mongo will not accted a document without title
     },
     description: String,
-    image:{ //for image if user did not give the image for air bnb we have a functionality to use default image  
-        type :String,
-        default : "https://images.app.goo.gl/HQRxunQEz2jRLCWT6" ,
-        set : (v)=> v=== "" ? "https://images.app.goo.gl/HQRxunQEz2jRLCWT6" : v 
+    image: { 
+        filename: String,
+        url: String
     },
     price: Number,  // Also corrected the typo "prince" -> "price"
     location: String,
